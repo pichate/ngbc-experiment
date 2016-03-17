82355a185e8157633a795234c78809ca92f80583
Pichate <pichate@kitchenup.com>
2016-03-17 16:57:16 +0700

    Initial Project and add type script compiler

---
+++ gulp/tsc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+var ts = require('gulp-typescript');
+
+gulp.task('ts-compile', function () {
+    return gulp.src(path.join(conf.paths.src, 'tsc/**/*.ts'))
+        .pipe(ts({
+            noImplicitAny: true,
+            target: 'es5'
+        }))
+        .pipe(gulp.dest(path.join(conf.paths.src, '/app/middlewares')));
+});
